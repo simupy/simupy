2ae8f2e7653f673e09567704772ebb7c3a39faac
ben <ben@sixpearls.com>
2016-07-23 07:34:11 -0700

    adding some documentation to matrix functions

--- NDMS/Matrices.py
+++ NDMS/Matrices.py
@@ -3,33 +3,56 @@ from sympy.physics.mechanics import dynamicsymbols
 from .Systems import DynamicalSystem
 from .utils import callable_from_trajectory
 
-def construct_explicit_matrix(name, n, m, symmetric=False, dynamic=False, **kwass):
+def construct_explicit_matrix(name, n, m, symmetric=False, diagonal=0, dynamic=False, **kwass):
+    """
+    args:
+    name - string base name for variables; each variable is name_ij, which admitedly only works clearly for n,m < 10
+    n - number of rows
+    m - number of columns
+    symmetric - use to enforce a symmetric matrix (repeat symbols above/below diagonal)
+    diagonal - zeros out off diagonals. takes precedence over symmetry. 
+    dynamic - use sympy.physics.mechanics.dynamicsymbol (defaults to sp.symbols)
+
+    kwargs: remaining kwargs passed to symbol function
+
+    returns sympy.Matrix of explicit symbolic elements
+    """
     if dynamic:
         symbol_func = dynamicsymbols
     else:
         symbol_func = sp.symbols
+
+    if n!=m and (diagonal or symmetric):
+        raise ValueError("Cannot make symmetric or diagonal if n != m")
     
-    matrix = sp.Matrix([ [ symbol_func(name+'_%d%d'%(j+1,i+1),**kwass) for i in range(m)] for j in range(n) ])
-    if not symmetric:
-        return matrix
-    elif n!=m:
-        raise ValueError("Cannot make symmetric if n != m")
+    if diagonal:
+        return sp.diag(*[ symbol_func(name+'_%d%d'%(i+1,i+1),**kwass) for i in range(m)])
     else:
-        for i in range(1,m):
-            for j in range(i):
-                matrix[i,j] = matrix[j,i]
+        matrix = sp.Matrix([ [ symbol_func(name+'_%d%d'%(j+1,i+1),**kwass) for i in range(m)] for j in range(n) ])
+
+        if symmetric:
+            for i in range(1,m):
+                for j in range(i):
+                    matrix[i,j] = matrix[j,i]
         return matrix
 
 def matrix_subs(*subs):
+    """
+    pass in 2-tuples of (symbolic,numeric) matrices OR dict of {symbolic: numeric} pairs
+    returns object that can be passed into sp.subs
+    """
     # I guess checking symmetry would be better, this will do for now.
-    if len(subs)==2:
+    if len(subs)==2 and not isinstance(subs[0],(list,tuple,dict)):
         subs = [subs]
     if isinstance(subs,(list,tuple)):
-        return tuple( (sub[0][i,j], sub[1][i,j]) for sub in subs for i in range(sub[0].shape[0]) for j in range(sub[0].shape[1]))
+        return tuple( (sub[0][i,j], sub[1][i,j]) for sub in subs for i in range(sub[0].shape[0]) for j in range(sub[0].shape[1]) if sub[0][i,j]!=0)
     elif isinstance(subs,dict):
-        return { sub[0][i,j]: sub[1][i,j] for sub in subs.items() for i in range(sub[0].shape[0]) for j in range(sub[0].shape[1]) }
+        return { sub[0][i,j]: sub[1][i,j] for sub in subs.items() for i in range(sub[0].shape[0]) for j in range(sub[0].shape[1]) if sub[0][i,j]!=0}
 
 def block_matrix(blocks):
+    """
+    Actually construct a block matrix, element by element
+    """
     return sp.Matrix.col_join(*tuple(sp.Matrix.row_join(*tuple(mat for mat in row)) for row in blocks))
     
 def matrix_callable_from_vector_trajectory(t,x,unraveled,raveled):
@@ -65,7 +88,7 @@ def matrix_callable_from_vector_trajectory(t,x,unraveled,raveled):
         return matrix_result
     return matrix_callable
             
-def system_from_matrix_DE(mat_DE, mat_var, mat_input=sp.Matrix([]), subs=[]):
+def system_from_matrix_DE(mat_DE, mat_var, mat_input=sp.Matrix([]), constants={}):
     # Sorry, not going to be clever and allow sets of DEs and variable matrices
     vec_var = list(set(sp.flatten(mat_var.tolist())))
     vec_DE = sp.Matrix.zeros(len(vec_var),1)
@@ -75,6 +98,6 @@ def system_from_matrix_DE(mat_DE, mat_var, mat_input=sp.Matrix([]), subs=[]):
         i,j = iterator.multi_index
         idx = vec_var.index(mat_var[i,j])
         vec_DE[idx] = mat_DE[i,j]
-        
-    sys = DynamicalSystem(vec_DE, sp.Matrix(vec_var), mat_input, constants_values=matrix_subs(subs))
+
+    sys = DynamicalSystem(vec_DE, sp.Matrix(vec_var), mat_input, constants_values=constants)
     return sys
