f46e1d89c613f09ac74c9c6e53626a5d91c93827
ben <ben@sixpearls.com>
2017-09-01 13:45:13 -0700

    fix logo to use in docs

--- docs/conf.py
+++ docs/conf.py
@@ -93,6 +93,10 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True
+}
+html_logo = '_static/simupy_color_small.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
